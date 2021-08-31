9e6b0d2d5d60128ab33b00528703d8c998e41503
sarathkumar.s <sarathkumar.s@optisolbusiness.com>
2021-08-31 16:26:59 +0530

    Column and Row Usage

---
+++ app/src/main/java/com/droid/jetpackcompose/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.droid.jetpackcompose
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.droid.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column {
+                Text(text = "Hello")
+                Text(text = "World")
+            }
+
+//            Row {
+//                Text(text = "Hello")
+//                Text(text = "World")
+//            }
+        }
+    }
+}
